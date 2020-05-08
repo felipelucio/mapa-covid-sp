78d070d8b8508024972769ca3c84ce777b6d0938
Felipe Lucio <felipeluc@gmail.com>
2020-05-08 17:13:05 -0300

    - adicionados datasets até 21/04. - adicionado script para junta-los. - modificado sistema de coloração e criado seletor de data.

--- map_viewer.js
+++ map_viewer.js
@@ -1,14 +1,35 @@
 var MapViewer = {
+    SELECT_COLOR: 'rgb(255,255,0)',
+    SCALE_COLORS: [
+        {'range': [0,0], 'color':'rgb(255,255,255)'}, 
+        {'range': [1,5], 'color': 'rgb(253,252,221)'},
+        {'range': [6,10], 'color': 'rgb(252,237,204)'}, 
+        {'range': [11,15], 'color': 'rgb(254,220,193)'},
+        {'range': [16,20], 'color': 'rgb(254,203,180)'}, 
+        {'range': [21,25], 'color': 'rgb(250,185,176)'}, 
+        {'range': [26,30], 'color': 'rgb(246,169,174)'},
+        {'range': [31,35], 'color': 'rgb(232,159,184)'},
+        {'range': [36,40], 'color': 'rgb(218,154,188)'},
+        {'range': [41,45], 'color': 'rgb(204,148,191)'},
+        {'range': [46,50], 'color': 'rgb(193,142,188)'},
+        {'range': [51,100], 'color': 'rgb(171,140,186)'}
+    ],
+    FONT_SIZE: 4,
+    DEFAULT_ZOOM: 10.2,
+    MIN_ZOOM: 9,
+    MAX_ZOOM: 13,
+
     _dataset: null,
     _available_dates: [],
     _selected_date: null,
     _selected_feature: null,
+    _dataset_range: { min: 0, max:0 },
 
     _map: null,
-    _choropleth_layer: null,
-    _choropleth_features: null,
     _selection_layer: null,
     _selection_features: null,
+    _text_layer: null,
+    _text_features: null,
 
     //======================
     // HTML elements
@@ -45,17 +66,79 @@ var MapViewer = {
             center: { x: -46.63644541794065, y: -23.669851138320273 },
             clampBoundsX: true,
             clampBoundsY: true,
-            zoom: 10.2,
-            max: 12,
-            min: 9
+            zoom: this.DEFAULT_ZOOM,
+            min: this.MIN_ZOOM,
+            max: this.MAX_ZOOM
         });
 
-        // create the layers
-        this._choropleth_layer = this._map.createLayer('feature');
+        // create the polygons layer
         this._selection_layer = this._map.createLayer('feature');
+        this._selection_layer.geoOn(geo.event.mouseclick, function(ev) {
+            that._selected_feature = null;
+            that._selection_features.modified();
+            that._selection_features.draw();
+            that.updateInfoBox(that._selected_feature);
+        });
+        this._selection_features = this._selection_layer
+            .createFeature('polygon', { selectionAPI: true })
+            .style({
+                fillColor: function (el, iel, data, idata) {
+                    if (data.properties.NOME_DIST == that._selected_feature) return that.SELECT_COLOR;
+
+                    var value = that.getValueById(data.properties.NOME_DIST);
+                    if (value) return that._getValueColor(value);
+                    return 'rgb(255,255,255)';
+                },
+                fillOpacity: 1,
+                stroke: true,
+                strokeOpacity: 1,
+                strokeWidth: 0.5,
+                strokeColor: 'rgb(80,80,80)'
+            })
+            .geoOff(geo.event.feature.mouseclick)
+            .geoOff(geo.event.feature.mouseover)
+            .geoOff(geo.event.feature.mouseout)
+            .geoOn(geo.event.feature.mouseclick, function (ev) {
+                that._selected_feature = ev.data.properties.NOME_DIST;
+                that.updateInfoBox(that._selected_feature);
+                this.modified();
+                this.draw();
+            })
+            .polygon(function (d) {
+                return {
+                    outer: d.geometry.coordinates[0]
+                }
+            })
+            .position(function (d) {
+                return { x: d[0], y: d[1] }
+            });
+        
+        // create the text layer
+        this._text_layer = this._map.createLayer('feature', {features:['text']});
+        this._text_features = that._text_layer.createFeature('text')
+            .style({
+                textScaled: 13
+            })
+            .position(function(d) {
+                var coordinates = d.geometry.coordinates[0].map(function(coord) {
+                    return {
+                        x: coord[0],
+                        y: coord[1],
+                        z: 0
+                    };
+                })
+                var center = geo.util.centerFromPerimeter(coordinates);
+                return {
+                    x: center.x,
+                    y: center.y
+                }
+            })
+            .text(function(district) {
+                return that.getDistrictName(district.properties.NOME_DIST);
+            });
 
         // add the event listener on the date slider
-        this._date_slider_el.addEventListener('change', function (ev) {
+        this._date_slider_el.addEventListener('input', function (ev) {
             var id = ev.srcElement.value;
             that.setSelectedDate(id);
             that._update();
@@ -63,56 +146,10 @@ var MapViewer = {
         });
 
         this._fetch('data/DEINFO_DISTRITO.json', function (map_data) {
-            // create the choropleth layer
-            that._choropleth_features = that._choropleth_layer
-                .createFeature('choropleth')
-                .data(map_data.features)
-                .choropleth({
-                    colorRange: ['rgb(255,255,255)', 'rgb(255,204,204)',
-                        'rgb(255,153,153)', 'rgb(255,102,102)', 'rgb(255,50,50)', 'rgb(255,0,0)']
-                })
-                .choropleth('geoId', function (district) { return district.properties.NOME_DIST });
-
-            // adds the mouseclick event on the selection_layer, unselecting all
-            that._selection_layer
-                .geoOn(geo.event.mouseclick, function (ev) {
-                    that._selected_feature = false;
-                    that._selection_features.modified();
-                    that._selection_features.draw();
-                });
-
             // adds the selection features
-            that._selection_features = that._selection_layer
-                .createFeature('polygon', { selectionAPI: true })
-                .data(map_data.features)
-                .style({
-                    fillColor: 'rgb(255,255,0)',
-                    fillOpacity: function (el, iel, data, idata) {
-                        if (data.properties.NOME_DIST == that._selected_feature) return 1;
-                        return 0;
-                    },
-                    stroke: true,
-                    strokeOpacity: 1,
-                    strokeWidth: 1,
-                    strokeColor: 'rgb(0,0,0)'
-                })
-                .geoOff(geo.event.feature.mouseclick)
-                .geoOff(geo.event.feature.mouseover)
-                .geoOff(geo.event.feature.mouseout)
-                .geoOn(geo.event.feature.mouseclick, function (ev) {
-                    that._selected_feature = ev.data.properties.NOME_DIST;
-                    that.updateInfoBox(that._selected_feature);
-                    this.modified();
-                    this.draw();
-                })
-                .polygon(function (d) {
-                    return {
-                        outer: d.geometry.coordinates[0]
-                    }
-                })
-                .position(function (d) {
-                    return { x: d[0], y: d[1] }
-                });
+            that._selection_features.data(map_data.features);
+            // add the text features
+            that._text_features.data(map_data.features);
 
             console.log('loading data')
             that._fetch('data/dataset.json', function (data) {
@@ -133,7 +170,8 @@ var MapViewer = {
                     el.setAttribute('label', date);
                     that._datelist_el.appendChild(el);
                 });
-
+                
+                that._calcDataRange('total');
                 that._update();
             });
         });
@@ -150,22 +188,14 @@ var MapViewer = {
     },
 
     _update: function() {
-        this._choropleth_features
-            .scalar(this.getSelectedDataset().map(function (district) {
-                return {
-                    value: district.total,
-                    id: district.id
-                }
-            }));
-        this._choropleth_features.modified();
-        this._choropleth_features.draw();
+        this._selection_features.modified();
+        this._text_features.modified();
         this._selection_features.draw();
-
+        this._text_features.draw();
 
         this.updateInfoBox(0);
     },
-
-    
+   
     updateInfoBox: function(feature_id) {
         var dataset = this.getSelectedDataset();
         var district = dataset.filter(function (el) {
@@ -175,6 +205,9 @@ var MapViewer = {
         if(district[0]) {
             this._info_box_title.textContent = this.getDistrictName(feature_id);
             this._info_box_total.textContent = district[0].total;
+        } else {
+            this._info_box_title.textContent = "";
+            this._info_box_total.textContent = "";
         }
     },
     
@@ -183,11 +216,47 @@ var MapViewer = {
     },
 
     getSelectedDataset: function() {
-        return this._dataset.dataset[this._selected_date];
+        return this._dataset.dataset[this._selected_date].data;
     },
 
     getDistrictName: function(NOME_DIST) {
         return this._dataset.names[NOME_DIST];
+    },
+
+    getValueById: function(NOME_DIST, value='total') {
+        var return_value = false;
+        if(this.getSelectedDataset())
+            this.getSelectedDataset().forEach(function(district) {
+                if(district.id == NOME_DIST)
+                    return_value = district[value];
+            });
+        
+        return return_value;
+    },
+
+    _calcDataRange: function(field) {
+        var that = this;
+        var max = 0;
+        var min = 0;
+        Object.keys(this._dataset.dataset).forEach(function(date) {
+            that._dataset.dataset[date].data.forEach(function(feature) {
+                if (feature[field] < min) min = feature[field];
+                if (feature[field] > max) max = feature[field];
+            });
+        })
+
+        this._dataset_range.min = min;
+        this._dataset_range.max = max;
+    },
+
+    _getValueColor: function(value) {
+        // if it is zero, return as the first color
+        for(i in this.SCALE_COLORS) {
+            if(value >= this.SCALE_COLORS[i].range[0] && value <= this.SCALE_COLORS[i].range[1])
+                return this.SCALE_COLORS[i].color;
+        }
+
+        return 'rgb(0,0,0)';
     }
 }
 
